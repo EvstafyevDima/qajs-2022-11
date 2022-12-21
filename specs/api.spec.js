89c5e3ffa1945396975022bd93e4d8cbe37a0f90
Evstafyev Dima <evstafyevdi@gmail.com>
2022-12-21 20:24:55 +0500

    api.spec.js

---
+++ specs/api.spec.js
@@ -0,0 +1,100 @@
+import axios from "axios";
+import {expect, test} from "@jest/globals";
+
+
+test('search for the couriers Id Login in the system', async () => {
+    const config = {
+        method: 'post',
+        url: 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login',
+        data: {
+            "login": "dnin55ja",
+            "password": "5555"
+          },
+    }
+        const resp = await axios(config);
+        console.log(resp);
+        expect(resp.status).toEqual(200);
+
+});
+
+test('Incorrect password when searching for the courier id', async () => {
+    const config = {
+        method: 'post',
+        url: 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login',
+        data: {
+            "login": "dnin55ja",
+            "password": "55554"
+          },
+    }
+
+    try {
+        const resp = await axios(config);
+    }
+    catch (e) {
+        console.log(e);
+        expect(e.response.status).toEqual(404);
+        expect(e.response.data.message).toEqual('Учетная запись не найдена')
+    }
+});
+
+test('Creating a duplicate of the couriers login', async () => {
+    const config = {
+        method: 'post',
+        url: 'https://qa-scooter.praktikum-services.ru/api/v1/courier',
+        data: {
+            "login": "dnin55ja",
+            "password": "5555",
+            "firstName": "saske"
+          },
+    }
+    try {
+        const resp = await axios(config);
+    }
+    catch (e) {
+        console.log(e);
+        expect(e.response.status).toEqual(409);
+        expect(e.response.data.message).toEqual('Этот логин уже используется')
+    }
+
+});
+
+test('Creating orders', async () => {
+    const config = {
+        method: 'post',
+        url: 'https://qa-scooter.praktikum-services.ru/api/v1/orders',
+        data: {
+            "firstName": "Dima",
+            "lastName": "DimaE",
+            "address": "Konwoha, 142 apt.",
+            "metroStation": 4,
+            "phone": "+7 900 355 35 35",
+            "rentTime": 5,
+            "deliveryDate": "2020-06-06",
+            "comment": "Saske, comwe back to Konoha",
+            "color": [
+                "BLACK"
+            ]
+          },
+    }
+        const resp = await axios(config);
+        console.log(resp);
+        expect(resp.status).toEqual(201);
+        expect(resp.data.track).toBeTruthy() 
+});
+
+test('Incorrect password when searching for the courier id', async () => {
+    const config = {
+        method: 'get',
+        url: 'https://qa-scooter.praktikum-services.ru/api/v1/courier/:704541/ordersCount',
+    }
+
+    try {
+        const resp = await axios(config);
+    }
+    catch (e) {
+        console.log(e);
+        expect(e.response.status).toEqual(404);
+        expect(e.response.data.message).toEqual('Not Found.')
+    }
+});
+
